153959de86a9d7cea0be6f828131f9a60e3e410c
deccer <decccer@outlook.com>
2021-01-05 20:18:15 +0400

    Add VertexBuffer

---
+++ src/OpenTK.Extended/Graphics/VertexBuffer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using OpenTK.Graphics.OpenGL4;
+
+namespace OpenTK.Extended.Graphics
+{
+    public class VertexBuffer : IDisposable
+    {
+        private readonly int _nativeHandle;
+
+        public static VertexBuffer Create<T>(T[] vertices)
+            where T : struct
+        {
+            var buffer = new VertexBuffer();
+            buffer.Initialize(vertices);
+            return buffer;
+        }
+
+        public static implicit operator int(VertexBuffer vertexBuffer)
+        {
+            return vertexBuffer._nativeHandle;
+        }
+
+        public int VertexStride { get; private set; }
+
+        public void Dispose()
+        {
+            GL.DeleteBuffer(_nativeHandle);
+        }
+
+        private VertexBuffer()
+        {
+            GL.GenBuffers(1, out _nativeHandle);
+        }
+
+        private void Initialize<T>(T[] vertices) where T : struct
+        {
+            VertexStride = Marshal.SizeOf<T>();
+            var size = vertices.Length * VertexStride;
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _nativeHandle);
+            GL.BufferData(BufferTarget.ArrayBuffer, size, vertices, BufferUsageHint.StaticDraw);
+        }
+    }
+}
